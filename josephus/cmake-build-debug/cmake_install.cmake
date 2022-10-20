de0414b318f7c1563d4268cdf045d6bb8b806128
TyxTang <2222002219@qq.com>
2022-10-20 15:28:26 +0800

    Fix File 22.10.20

--- josephus/cmake-build-debug/cmake_install.cmake
+++ josephus/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Project/CProject/josephus
+# Install script for directory: E:/Project/CProject/MyC-Project/josephus
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Project/CProject/josephus/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/Project/CProject/MyC-Project/josephus/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
